299bd8f050a8edcd67f05a847336c0343c60ccc1
h4rdPL <mateusz.mamica18@gmail.com>
2022-10-30 16:26:43 +0100

    adding mobile menu styles and javascript

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("hey!");
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const handleClick = () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("bar-active");
+};
+
+hamburger.addEventListener("click", handleClick);
