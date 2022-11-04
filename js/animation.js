5475d438bd3d9fe0a054b8dfb0ad49c3362f5573
h4rdPL <mateusz.mamica18@gmail.com>
2022-11-04 12:26:55 +0100

    adding animation for contact section, removing event listener from gsap animation

--- js/animation.js
+++ js/animation.js
@@ -1,18 +1,21 @@
-//
+
 const section = document.getElementsByClassName("grid");
-// gsap.registerPlugin(ScrollTrigger);
-window.addEventListener("load", () => {
-  gsap.from("#nav", {
-    opacity: 0,
-    y: -10,
-    duration: 1,
-  });
-  gsap.from("#hero", {
-    opacity: 0,
-    y: -100,
-    delay: 0.5,
-    duration: 0.5,
-  });
+const contact = document.getElementById("contact");
+gsap.from("#nav", {
+  opacity: 0,
+  y: -10,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#hero",
+    start: "top top"
+  }
+});
+gsap.from("#hero", {
+  opacity: 0,
+  y: -100,
+  delay: 0.5,
+  duration: 1,
+
 });
 // project secion animation
 gsap.fromTo(
@@ -31,3 +34,19 @@ gsap.fromTo(
     },
   }
 );
+gsap.fromTo(
+  contact.children,
+  {
+    y: "+=100",
+    opacity: 0
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+      trigger: contact,
+      start: "top center"
+    }
+  }
+)
