dbb5f3bd24365425e11c47c54ff5a9a66fe02fb0
h4rdPL <mateusz.mamica18@gmail.com>
2023-01-30 12:11:59 +0100

    update contact and project section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
+const submit = document.getElementById("send-btn");
+const subject = document.getElementById("subject");
+const body = document.getElementById("body");
+const email = document.getElementById("email");
 // hamburger
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+
 const handleClick = () => {
   menu.classList.toggle("active");
   hamburger.classList.toggle("bar-active");
 };
 
+const handleSendEmail = (e) => {
+  e.preventDefault();
+  const messageParams = {
+    from_email: email.value,
+    to_name: "mateusz.mamica18@gmail.com",
+    message: body.value,
+  };
+  emailjs.send("service_06yt80v", "template_ir8wxou", messageParams).then(
+    function (res) {
+      console.log(res);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+};
+
 hamburger.addEventListener("click", handleClick);
+//smtp -> send email
+
+submit.addEventListener("click", handleSendEmail);
