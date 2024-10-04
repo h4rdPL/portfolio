e1e4fb3225a5aa46f2634650d9f7664141c18867
Mateusz Mamica <mateusz.mamica18@gmail.com>
2024-10-04 11:32:47 +0200

    Update form validation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const submit = document.getElementById("send-btn");
 const subject = document.getElementById("subject");
 const body = document.getElementById("body");
 const email = document.getElementById("email");
-// hamburger
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const messageStatus = document.getElementById("message-status");
@@ -14,6 +13,33 @@ const handleClick = () => {
 
 const handleSendEmail = (e) => {
   e.preventDefault();
+  if (!email.value.trim()) {
+    messageStatus.textContent = "Please enter an email address.";
+    messageStatus.style.display = "block";
+    messageStatus.style.color = "red";
+    return;
+  }
+  if (!email.value.includes("@")) {
+    messageStatus.textContent = "Please enter a valid email address.";
+    messageStatus.style.display = "block";
+    messageStatus.style.color = "red";
+    return;
+  }
+
+  if (!subject.value.trim()) {
+    messageStatus.textContent = "Please enter a subject.";
+    messageStatus.style.display = "block";
+    messageStatus.style.color = "red";
+    return;
+  }
+
+  if (!body.value.trim()) {
+    messageStatus.textContent = "Please enter a message.";
+    messageStatus.style.display = "block";
+    messageStatus.style.color = "red";
+    return;
+  }
+
   const messageParams = {
     subject: subject.value,
     from_email: email.value,
@@ -30,7 +56,7 @@ const handleSendEmail = (e) => {
         origin: { x: 0.5, y: 0.5 },
       });
 
-      messageStatus.textContent = "Wiadomość została wysłana!";
+      messageStatus.textContent = "The message has been sent!";
       messageStatus.style.display = "block";
     },
     function (error) {
